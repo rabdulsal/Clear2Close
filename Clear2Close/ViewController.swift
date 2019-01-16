0def20d20af72bd3de91ee8ee29f7f068b1cd863
Rashad Abdul-Salaam <rashad@getskinclick.com>
2019-01-16 17:15:47 -0500

    Initial Commit

---
+++ Clear2Close/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clear2Close
+//
+//  Created by Rashad Abdul-Salaam on 1/16/19.
+//  Copyright © 2019 Clear2Close. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
