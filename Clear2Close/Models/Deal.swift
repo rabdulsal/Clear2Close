dac3cae04ac9d3f5c5db3dd6350deed78d586419
Rashad Abdul-Salaam <rashad@getskinclick.com>
2019-01-21 10:31:02 -0500

    Initial commit w/ gitignore

---
+++ Clear2Close/Models/Deal.swift
@@ -0,0 +1,9 @@
+//
+//  Deal.swift
+//  Clear2Close
+//
+//  Created by Rashad Abdul-Salaam on 1/16/19.
+//  Copyright © 2019 Clear2Close. All rights reserved.
+//
+
+import Foundation
---
+++ Clear2Close/Services/AnalysisService.swift
@@ -0,0 +1,9 @@
+//
+//  AnalysisService.swift
+//  Clear2Close
+//
+//  Created by Rashad Abdul-Salaam on 1/16/19.
+//  Copyright © 2019 Clear2Close. All rights reserved.
+//
+
+import Foundation
