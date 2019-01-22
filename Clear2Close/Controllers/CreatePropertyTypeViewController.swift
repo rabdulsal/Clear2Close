e6b9e9a72ad8f7dff6634f81f634a9a628aab98f
University of Pennsylvania <upennmobileapps@gmail.com>
2019-01-22 09:33:46 -0500

    Basic setup, UI flow  & provisioning profile configured

---
+++ Clear2Close/Controllers/CreatePropertyTypeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreatePropertyTypeViewController.swift
+//  Clear2Close
+//
+//  Created by Rashad Abdul-Salam on 1/21/19.
+//  Copyright © 2019 Clear2Close. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreatePropertyTypeViewController : UIViewController {
+    /*
+     --- BIG NOTES ---
+     1. TextField for address entry (exclude city/state/zip for now)
+     2. Buttons for 'Rental' and 'Flip' -- disabled until TextField complete
+     */
+}
---
+++ Clear2Close/Controllers/MyPropertiesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyPropertiesViewController.swift
+//  Clear2Close
+//
+//  Created by Rashad Abdul-Salam on 1/21/19.
+//  Copyright © 2019 Clear2Close. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyPropertiesViewController : UIViewController {
+    /*
+     --- BIG NOTES ---
+     1. Must conform to SearchBar delegate
+     2. Needs Section headers for 'Rentals' 'Flips' 'Other', that toggle mini-table views when header is tapped
+     3. Needs Empty Screen when there are no properties created
+    */
+}
